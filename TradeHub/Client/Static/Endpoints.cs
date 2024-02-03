8b473b384d57f443717de3fab2a44ca42e6d66ee
GOH ZONG HAN KEVIN 2102632C <2102632C@student.tp.edu.sg>
2024-02-03 18:04:57 +0800

    Customer Page created, settedn up folders for individual page. (Customer page not complete)

---
+++ TradeHub/Client/Static/Endpoints.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradeHub.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+        public static readonly string SellOrdersEndpoint = $"{Prefix}/sellorders";
+        public static readonly string TradeOrdersEndpoint = $"{Prefix}/tradeorders";
+        public static readonly string ProductsEndpoint = $"{Prefix}/products";
+    }
+}
